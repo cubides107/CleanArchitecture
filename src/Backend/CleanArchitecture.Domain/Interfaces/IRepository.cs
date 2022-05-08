37e84d7e5f967b38016e685ecccfd5ca6ec73be9
cubides107 <cristian.cubides@uptc.edu.co>
2022-05-08 11:30:21 -0500

    F-Jerarquia de carpetas para capa de dominio e infraesctrutura

---
+++ src/Backend/CleanArchitecture.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Interfaces
+{
+    public interface IRepository
+    {
+        public Task Save<T>(T obj) where T : Entity;
+    }
+}

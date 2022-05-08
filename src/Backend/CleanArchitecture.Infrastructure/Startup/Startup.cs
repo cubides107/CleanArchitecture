64499612a0170e6f104a58d7aa523f26681060c0
cubides107 <cristian.cubides@uptc.edu.co>
2022-05-08 11:57:21 -0500

    F-Configuracion para injeccion de dependencias

--- src/Backend/CleanArchitecture.Infrastructure/Startup/Startup.cs
+++ src/Backend/CleanArchitecture.Infrastructure/Startup/Startup.cs
@@ -1,4 +1,6 @@
-﻿using CleanArchitecture.Infrastructure.EF;
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Infrastructure.EF;
+using CleanArchitecture.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,11 +17,26 @@ namespace CleanArchitecture.Infrastructure.Startup
         public static void Start(IServiceCollection services, IConfiguration configuration)
         {
             ConfigureContext(services, configuration);
+            ConfigureDependencyInjection(services);
         }
 
+        /// <summary>
+        /// Metodo para configurar el contexto
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
         public static void ConfigureContext(IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<CleanArchitectureContext>(options => options.UseSqlServer(configuration.GetConnectionString("ArchitectureContext")));
+        }
+
+        /// <summary>
+        /// Metodo para configurar la injeccion de dependencias
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureDependencyInjection(IServiceCollection services)
+        {
+            services.AddScoped<IRepository, SQLServerRepository>();
         }
 
     }
